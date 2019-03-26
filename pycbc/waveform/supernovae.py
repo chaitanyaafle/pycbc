3153528cbea4dc41f2c5af10b76b2800c3a4814f
Chaitanya Afle <aflechaitanya@gmail.com>
2019-03-25 20:04:19 -0400

    added the waveform generating function (initial commit)

--- pycbc/waveform/supernovae.py
+++ pycbc/waveform/supernovae.py
@@ -6,8 +6,14 @@ import h5py
 from pycbc.types import TimeSeries, FrequencySeries, float64, complex128, zeros
 from pycbc.waveform.waveform import get_obj_attrs
 
-def get_td_corecollapse_bounce_signal():
-    pass
+def get_td_corecollapse_bounce_signal(coefficients_array, pc_hdf_file):
+    """
+    """
+    pc_file = h5py.File(pc_hdf_file, 'r')
+    principal_components = numpy.array(pc_file.get('principal_components'))
+
+    strain = numpy.dot(coeffs, pc_test)
+    return strain
 
 # Approximant names ###########################################################
 supernovae_td_approximants = {'CoreCollapseBounce' : get_td_corecollapse_bounce_signal}
